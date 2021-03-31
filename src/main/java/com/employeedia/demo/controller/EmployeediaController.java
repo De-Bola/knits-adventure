486c13c66fec0954b154223cc5e5d7be11a61eac
Adebola Gbiri <adebolaolubunmi32@gmail.com>
2021-03-31 09:07:03 +0300

    first commit

---
+++ src/main/java/com/employeedia/demo/controller/EmployeediaController.java
@@ -0,0 +1,69 @@
+package com.employeedia.demo.controller;
+
+import com.employeedia.demo.model.Employee;
+import com.employeedia.demo.service.EmployeediaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+// http://localhost:8080
+@CrossOrigin(origins = {"http://localhost:3001", "http://localhost:3000"})
+@RestController
+public class EmployeediaController {
+
+    private final EmployeediaService employeediaService;
+
+    public EmployeediaController(EmployeediaService employeediaService) {
+        this.employeediaService = employeediaService;
+    }
+
+    //fetch all from db
+    @GetMapping("/api/employeediaItems")
+    public ResponseEntity<?> fetchAllEmployeeItems(){
+        Iterable<Employee> employeeItems = employeediaService.getAllEmployees();
+
+        return ResponseEntity.ok(employeeItems);
+    }
+
+    @PostMapping("/api/add-employee")
+    public Employee addEmployee(@RequestBody Employee employee){
+        return employeediaService.addEmployee(employee);
+    }
+
+    @PutMapping("/api/edit-employee/{id}")
+    public ResponseEntity<?> editEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        if(employeediaService.getEmployeeById(id).isPresent()){
+            try{
+                employeediaService.addEmployee(employee);
+                return new ResponseEntity<>(HttpStatus.OK);
+            } catch (Exception e){
+                e.getMessage();
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/api/delete-employee/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+        if(employeediaService.getEmployeeById(id).isPresent()){
+            employeediaService.deleteEmployee(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/api/delete-employees/all")
+    public ResponseEntity<?> deleteEmployees(){
+        try{
+            employeediaService.deleteEmployees();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e){
+            e.getMessage();}
+        finally {
+            new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+}
---
+++ src/main/java/com/employeedia/demo/model/Employee.java
@@ -0,0 +1,29 @@
+package com.employeedia.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+    private Date hireDate;
+    private boolean isActive;
+
+}
